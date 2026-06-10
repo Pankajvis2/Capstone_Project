c3b9fe493b48ddabb0531c4262b5a00821c2640a
Pankajvis1 <pankajvishwakarma@localhost>
2026-06-10 11:13:52 +0530

    docker fix

--- Capestone_Project_ParaBank/src/test/java/factory/DriverFactory.java
+++ Capestone_Project_ParaBank/src/test/java/factory/DriverFactory.java
@@ -2,6 +2,7 @@ package factory;
 
 import config.ConfigReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -14,57 +15,81 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DriverFactory {
-	private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-	public static void initDriver(String browser) {
-		Logger.getLogger("org.openqa.selenium").setLevel(Level.SEVERE);
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-		boolean headless = Boolean.parseBoolean(ConfigReader.get("headless"));
-		String browserName = browser == null ? "chrome" : browser.toLowerCase().trim();
+    public static void initDriver(String browser) {
 
-		switch (browserName) {
-		case "safari":
-			if (headless) {
-				System.out.println("Safari does not support headless mode. Launching Safari in normal mode.");
-			}
-			driver.set(new SafariDriver());
-			break;
+        Logger.getLogger("org.openqa.selenium").setLevel(Level.SEVERE);
 
-		case "firefox":
-			WebDriverManager.firefoxdriver().setup();
-			FirefoxOptions firefoxOptions = new FirefoxOptions();
-			if (headless) {
-				firefoxOptions.addArguments("--headless");
-			}
-			driver.set(new FirefoxDriver(firefoxOptions));
-			break;
+        boolean headless = Boolean.parseBoolean(ConfigReader.get("headless"));
+        String browserName = browser == null ? "chrome" : browser.toLowerCase().trim();
 
-		case "chrome":
-		default:
-			WebDriverManager.chromedriver().setup();
-			ChromeOptions chromeOptions = new ChromeOptions();
-			if (headless) {
-				chromeOptions.addArguments("--headless=new");
-			}
-			chromeOptions.addArguments("--start-maximized");
-			chromeOptions.addArguments("--disable-notifications");
-			chromeOptions.addArguments("--remote-allow-origins=*");
-			driver.set(new ChromeDriver(chromeOptions));
-			break;
-		}
+        switch (browserName) {
 
-		getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(ConfigReader.getInt("implicitWait")));
-		getDriver().manage().window().maximize();
-	}
+            case "safari":
+                if (headless) {
+                    System.out.println("Safari does not support headless mode. Launching Safari in normal mode.");
+                }
+                driver.set(new SafariDriver());
+                break;
 
-	public static WebDriver getDriver() {
-		return driver.get();
-	}
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
 
-	public static void quitDriver() {
-		if (driver.get() != null) {
-			driver.get().quit();
-			driver.remove();
-		}
-	}
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+                if (headless || isRunningInsideDocker()) {
+                    firefoxOptions.addArguments("--headless");
+                }
+
+                driver.set(new FirefoxDriver(firefoxOptions));
+                break;
+
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+
+                ChromeOptions chromeOptions = new ChromeOptions();
+
+                if (headless || isRunningInsideDocker()) {
+                    chromeOptions.addArguments("--headless=new");
+                }
+
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--window-size=1920,1080");
+                chromeOptions.addArguments("--disable-notifications");
+                chromeOptions.addArguments("--remote-allow-origins=*");
+
+                driver.set(new ChromeDriver(chromeOptions));
+                break;
+        }
+
+        getDriver().manage().timeouts()
+                .implicitlyWait(Duration.ofSeconds(ConfigReader.getInt("implicitWait")));
+
+        if (!isRunningInsideDocker()) {
+            getDriver().manage().window().maximize();
+        } else {
+            getDriver().manage().window().setSize(new Dimension(1920, 1080));
+        }
+    }
+
+    private static boolean isRunningInsideDocker() {
+        String osName = System.getProperty("os.name").toLowerCase();
+        return osName.contains("linux");
+    }
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
+        }
+    }
 }
